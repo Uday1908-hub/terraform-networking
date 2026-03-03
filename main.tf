f5855338877f1ebeda977e1734a63a622fbbffab
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-03-03 14:17:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,28 +13,6 @@ terraform {
  }
 }
 provider "azurerm" {
- features {}  # Required
+ features {}  
 }
-# Resource Group
-resource "azurerm_resource_group" "rg" {
- name     = "rg-demo-network"
- location = "East US"
-}
-# Virtual Network
-resource "azurerm_virtual_network" "vnet" {
- name                = "vnet-demo"
- location            = azurerm_resource_group.rg.location
- resource_group_name = azurerm_resource_group.rg.name
- address_space       = ["10.0.0.0/16"]
-}
-# Subnet
-resource "azurerm_subnet" "subnet" {
- name                 = "subnet-demo"
- resource_group_name  = azurerm_resource_group.rg.name
- virtual_network_name = azurerm_virtual_network.vnet.name
- address_prefixes     = ["10.0.1.0/24"]
-}
-# Outputs (for compute workspace)
-output "rg_name" { value = azurerm_resource_group.rg.name }
-output "subnet_id" { value = azurerm_subnet.subnet.id }
-output "location" { value = azurerm_resource_group.rg.location }
+
